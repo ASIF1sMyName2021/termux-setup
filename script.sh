1d0314180eca262474134315ca2897dea8f74087
ASIF1sMyName2021 <79853297+ASIF1sMyName2021@users.noreply.github.com>
2022-04-06 22:58:46 +0600

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# Termux Easy Setup Script
+
+# Clear the terminal
+clear
+
+# Welcome Text 
+printf "\e[1;31m ┌──────────────────────────┐ \e[0m\n" 
+printf "\e[1;31m │ Termux Easy Setup Script │ \e[0m\n"
+printf "\e[1;31m └──────────────────────────┘ \e[0m\n"
+printf ""
+
+# Setup the termux storage
+termux-setup-storage
+rm -r ~/storage
+
+# Update the temux repository
+pkg update
+
+# Install micro text editor
+pkg install micro
+
+# Clear the terminal
+clear
+
+# Setup the .termux directory 
+\cp files/termux.properties ~/.termux/
+\cp files/colors.properties ~/.termux/
+\cp files/font.ttf ~/.termux/
+
+# Change the Termux welcome text
+\cp files/motd ~/../usr/etc/
+
+# Change the shell prompt style
+\cp files/bash.bashrc ~/../usr/etc/
+
+# Install Proot Distro
+pkg install proot-distro
+proot-distro install ubuntu
+
+# Clear the terminal
+clear
+
+# Completion Message
+printf "\e[1;31m ◆◆ Setup Complete ◆◆ \e[0m\n"
+printf "\e[1;31m -------------------- \e[0m\n"
+printf "\e[1;32m Restart Termux for changes to take effect. \e[0m\n"
+printf ""
